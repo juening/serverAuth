dcd8bf21f91cf972ba4c67bdd135fb70a7c96a12
Jue Ning <juening@gmail.com>
2017-04-06 19:38:00 -0400

    bcrypt password before saving to mongo

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const http = require('http');
+const bodyParser = require('body-parser');
+const morgan  = require('morgan');
+const mongoose = require('mongoose');
+
+//db
+mongoose.connect('mongodb://localhost:auth/auth');
+
+const app= express();
+const router = require('./router');
+//App setup
+app.use(morgan('combined'));
+app.use(bodyParser.json({ type: '*/*'}));
+router(app);
+
+//Server setup
+const port = process.env.PORT || 3090;
+const server = http.createServer(app);
+server.listen(port);
+console.log('Server is listening to ', port);
